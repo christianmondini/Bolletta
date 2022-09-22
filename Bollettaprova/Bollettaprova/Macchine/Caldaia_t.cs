6f1e6a698028a815df7b76120cafb23b2cfd1a43
christian-mondini <christian.mondini@iisviolamarchesini.edu.it>
2022-09-22 22:36:15 +0200

    3

---
+++ Bollettaprova/Bollettaprova/Macchine/Caldaia_t.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bollettaprova.Macchine
+{
+    class Caldaia_t:Macchine
+    {
+    }
+}
---
+++ Bollettaprova/Bollettaprova/Macchine/Pompa_e.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bollettaprova.Macchine
+{
+    class Pompa_e:Macchine
+    {
+    }
+}
