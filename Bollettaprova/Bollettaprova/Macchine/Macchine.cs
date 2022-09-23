44b5985d527c282805fc0b70035b29cb5baa6bc5
christianmondini <90891659+christianmondini@users.noreply.github.com>
2022-09-23 08:55:03 +0200

    Variabili Macchine

--- Bollettaprova/Bollettaprova/Macchine/Macchine.cs
+++ Bollettaprova/Bollettaprova/Macchine/Macchine.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Bollettaprova.Macchine
 {
+    Protected string tipo_consumo;
+    Protected double materia;
+    Protected double costi_aggiuntivi;
     class Macchine
     {
     }
